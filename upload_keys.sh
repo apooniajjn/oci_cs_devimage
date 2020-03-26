6e034f2e92a6acdac9d587300e395ec3eaa31842
andytael <andy.tael@yahoo.com>
2020-03-26 11:34:16 -0500

    Changes

--- upload_keys.sh
+++ upload_keys.sh
@@ -9,15 +9,17 @@ fi
 oci os bucket create --namespace ${os_namespace} --name ${bucket_name} --compartment-id $ocid_comp
 oci os object put -bn ${bucket_name} --file ${private_key}
 oci os object put -bn ${bucket_name} --file ${public_key}
-access_uri_private=$(oci os preauth-request create --access-type ObjectRead \ 
+preauth_private=$(oci os preauth-request create --access-type ObjectRead \ 
     -bn ${bucket_name} --name Keys \
     --time-expires date --date=`'tomorrow' +%Y-%m-%d` \
     -on id_rsa \
     --query 'data[0]."access-uri"')
-access_uri_public=$(agreement=$(oci os preauth-request create --access-type ObjectRead \
+preauth_public=$(oci os preauth-request create --access-type ObjectRead \
     -bn ${bucket_name} --name Keys --time-expires \
     `date --date=`'tomorrow' +%Y-%m-%d` \
     -on id_rsa.pub \
     --query 'data[0]."access-uri")
 
 # Need id for deletion
+# --query 'data[?contains("display-name", `'"${image_name}"'`)].join('"'"' '"'"', ["listing-id", "display-name"]) | join(`\n`, @)' \
+#  ; read ocid_listing display_name <<< "${pic_listing}"
